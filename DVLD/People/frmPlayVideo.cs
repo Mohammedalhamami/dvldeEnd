7e7f63a80ef2cf1b4b5cda50fb12baadff1544f0
Mohammed Haider <mohammed.haider.alhamami1999@gmail.com>
2024-02-10 15:36:18 +0300

    Added play video form in people file

---
+++ DVLD/People/frmPlayVideo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DVLD.People
+{
+    public partial class frmPlayVideo : Form
+    {
+        public frmPlayVideo()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            //wmpProject.Play
+            string VideoPath = @"C:\Users\Windows10\Desktop\20230922-224340.mp4";
+
+            wmpProject.URL = VideoPath;
+            wmpProject.Ctlcontrols.play();
+
+        }
+    }
+}
